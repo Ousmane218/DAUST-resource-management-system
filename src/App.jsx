275b616f5dc0b11459484f3a204dfcaf0e69a23b
Ousmane <osoumare@mydaust.org>
2025-12-10 14:16:37 +0000

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Hello World babies</h1>
+      <h1 className="text-4xl font-bold text-blue-600">Hello Ndeye Marieme is my beloved wife</h1>
     </div>
   )
 }
